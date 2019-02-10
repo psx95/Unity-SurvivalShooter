f2fb057cb0117c2ad48d77e4ac1a955fc204abe0
psx95 <pranav.ps95@hotmail.com>
2019-02-10 18:59:05 +0530

    Create Camera Follow

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothing = 5f;
+
+    Vector3 offset; //offset between camera and player
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - target.position; 
+    }
+
+    // Update is called once per frame
+    /*void Update()
+    {
+        
+    }*/
+
+    // Since we are following a Physics Object
+    private void FixedUpdate()
+    {
+        Vector3 targetCamPos = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+    }
+}
